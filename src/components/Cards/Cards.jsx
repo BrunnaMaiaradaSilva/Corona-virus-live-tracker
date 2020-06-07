02a39f90d3437cc8a1cd6d1e8207402485d8fcae
Brunna Silva <silva.brunnam@gmail.com>
2020-06-07 20:44:55 -0300

    Cards Confirmados, Recuperados e Mortes

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,18 +3,35 @@ import { Card, CardContent, Typography, Grid, StylesProvider } from '@material-u
 
 import styles from './Cards.module.css';
 
-const Cards = (props) => {
-    console.log(props);
-
+const Cards = ({ data: { confirmed, recovered, deaths,lastUpdate } }) => {
+    if(!confirmed){
+        return 'Carregando...';
+    }
     return (
         <div className={StylesProvider.container}>
             <Grid container={3} justify="center">
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infectados</Typography>
-                        <Typography variant="h5">Dados reais</Typography>
+                        <Typography variant="h5">{confirmed.value}</Typography>
                         <Typography color="textSecondary">Data atual</Typography>
                         <Typography variant="body2">Casos ativos de COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recuperados</Typography>
+                        <Typography variant="h5">{recovered.value}</Typography>
+                        <Typography color="textSecondary">Data atual</Typography>
+                        <Typography variant="body2">Número de casos de recuperação COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Mortes</Typography>
+                        <Typography variant="h5">{deaths.value}</Typography>
+                        <Typography color="textSecondary">Data atual</Typography>
+                        <Typography variant="body2">Número de mortes de COVID-19</Typography>
                     </CardContent>
                 </Grid>
             </Grid>
